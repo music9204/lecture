900505ffcdde8158c6b587fa59b3f2e56baef93f
music9204 <music9204@gmail.com>
2026-06-19 15:56:57 +0900

    2 remove 숙제

--- 26.06.16/src/components/TodoList/index.jsx
+++ 26.06.16/src/components/TodoList/index.jsx
@@ -2,12 +2,17 @@ import { TodoItem } from './TodoItem';
 import styles from './index.module.css';
 
 export const TodoList = (props) => {
-  const { todos, handleToggle } = props;
+  const { todos, handleToggle, handleRemove } = props;
 
   return (
     <div className={styles.todoList}>
       {todos.map((todo) => (
-        <TodoItem todo={todo} key={todo.id} handleToggle={handleToggle} />
+        <TodoItem
+          todo={todo}
+          key={todo.id}
+          handleToggle={handleToggle}
+          handleRemove={handleRemove}
+        />
       ))}
     </div>
   );
